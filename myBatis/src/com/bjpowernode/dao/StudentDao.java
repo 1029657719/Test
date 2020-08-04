484cecbc2244473ff08b159957dbff9c0fe36651
zhangjia <1029657719@qq.com>
2020-08-04 09:36:49 +0800

    修改成300后pull 看看能不能pull下来

--- myBatis/src/com/bjpowernode/dao/StudentDao.java
+++ myBatis/src/com/bjpowernode/dao/StudentDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 //这个接口是与Mapping连接的接口，由StudentDaoImpl去具体实现
 public interface StudentDao {
      List<Student> getAll();
-     int A = 200;
+     int B = 300;
     public Student selectStudents(String id);
 
     public void insertStudent(Student stu);
